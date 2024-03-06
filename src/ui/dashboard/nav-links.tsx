b0226647e2f2a3ec653163e32edbac70d44f2ee5
王洋 <dayang.wy@raycloud.com>
2024-03-06 11:06:19 +0800

    ✨ feat(模板更换):
    https://github.com/vercel/next-learn/tree/main/dashboard/starter-example/app/ui/dashboard

--- src/ui/dashboard/nav-links.tsx
+++ src/ui/dashboard/nav-links.tsx
@@ -1,24 +1,36 @@
 'use client';
-// import {
-//   UserGroupIcon,
-//   HomeIcon,
-//   DocumentDuplicateIcon,
-// } from '@heroicons/react/24/outline';
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
+
 export default function NavLinks() {
   const pathname = usePathname();
 
-  const links: Array<{name: string, href: string, icon: any}> = [
-    { name: 'customer', href: '/dashboard/customers', icon: '' },
-    { name: 'invoices', href: '/dashboard/invoices', icon: '' },
-  ]
+  // const links: Array<{name: string, href: string, icon: any}> = [
+  //   { name: 'customer', href: '/dashboard/customers', icon: '' },
+  //   { name: 'invoices', href: '/dashboard/invoices', icon: '' },
+  // ]
   return (
     <>
       {links.map((link) => {
-        // const LinkIcon = link.icon;
+        const LinkIcon = link.icon;
         return (
           <Link
             key={link.name}
@@ -30,7 +42,7 @@ export default function NavLinks() {
               },
             )}
           >
-            {/* <LinkIcon className="w-6" /> */}
+            <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
         );
