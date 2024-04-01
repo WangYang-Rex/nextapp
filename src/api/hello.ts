c089d9c9661b3026b27f401f65c3c51b591c95c0
王洋 <dayang.wy@raycloud.com>
2024-04-01 17:39:59 +0800

    ✨ feat: 删除api-hello

--- src/api/hello.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
-  const data = { header: req.headers, url: req.url, text: "返回内容" };
-  res.status(200).json(data);
-}
